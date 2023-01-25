1c348920288fbb435323f12bfeaad7360355de8b
Jinny <ppwm111@naver.com>
2023-01-25 14:39:54 +0900

    폴더 구조

--- client.tsx
+++ client.tsx
@@ -1,5 +1,16 @@
 import App from '@layouts/App';
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-render(<App />, document.querySelector('#app'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
+
+// 폴더구조는 자유
+// pages -> 페이지별(회원가입 페이지, 로그인 페이지, )
+// components -> 짜잘한 컴포넌트들
+// layouts -> 공통 레이아웃
